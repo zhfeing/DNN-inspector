4ea8b744d8f59635aba697c3cc7e3c5586c3bd51
zhfeing <zhf666968@gmail.com>
2022-05-13 14:06:41 +0800

    added vis loss landscape

--- visualization/vis_loss_landscape.py
+++ visualization/vis_loss_landscape.py
@@ -6,7 +6,10 @@ import logging
 import signal
 import traceback
 import argparse
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Callable
+
+import matplotlib.pyplot as plt
+import numpy as np
 
 import torch
 import torch.backends.cudnn
@@ -18,12 +21,16 @@ from cv_lib.config_parsing import get_cfg
 
 from utils import ModelWrapper
 from utils.task_scheduler import GPUTaskScheduler
+from utils.accumulator import EvalAccumulators
+
 from driver.data import build_train_dataloader
 from driver.loss import get_loss_fn, l2_norm, l1_norm
 from driver.eval import Evaluation
+
 from dnn_inspector.loss_landscape.direction import setup_direction
 from dnn_inspector.loss_landscape.surface import setup_coordinates
 from dnn_inspector.loss_landscape.model_setter import set_weights
+from dnn_inspector.loss_landscape.gerenate_vtk import generate_vtp
 
 
 START_METHOD = "spawn"
@@ -113,13 +120,112 @@ def logger_constructor(logger_fp: str):
     return logger, logger_fp
 
 
-def plot(args):
-    eval_path = os.path.join(args.save_path, "eval")
-    eval_files = os.listdir(eval_path)
-    for filename in eval_files:
-        fp = os.path.join(eval_path, filename)
-        result = torch.load(fp, map_location="cpu")
-        print(result)
+def plot_landscape(
+    save_path: str,
+    x_coordinate: np.ndarray,
+    y_coordinate: np.ndarray,
+    transformers: Dict[str, Callable[[np.ndarray], np.ndarray]],
+    values: Dict[str, np.ndarray],
+    levels: int = 10,
+    dpi: int = 300,
+    # vtp_cfg: Dict[str, Any],
+):
+    x_axis, y_axis = np.meshgrid(x_coordinate, y_coordinate)
+    assert len(x_coordinate) > 1 and len(y_coordinate) > 1, "x or y coordinates must more than one value"
+
+    # plot 2D contours
+    def plot_value(z: np.ndarray, save_fp: str, levels: int, cmap: str = "RdGy"):
+        contour = plt.contour(
+            x_axis, y_axis, z,
+            cmap=cmap,
+            levels=levels
+        )
+        plt.clabel(contour, inline=True, fontsize=8)
+        plt.savefig(
+            save_fp.format(name="contour"),
+            dpi=dpi,
+            bbox_inches="tight"
+        )
+        plt.close()
+
+        plt.contourf(
+            x_axis, y_axis, z,
+            cmap=cmap,
+            levels=levels
+        )
+        plt.colorbar()
+        plt.savefig(
+            save_fp.format(name="contourf"),
+            dpi=dpi,
+            bbox_inches="tight"
+        )
+        plt.close()
+
+        _, ax = plt.subplots(subplot_kw={"projection": "3d"})
+        ax.plot_surface(
+            x_axis, y_axis, z,
+            cmap=cmap,
+            linewidth=0,
+            antialiased=False
+        )
+        plt.savefig(
+            save_fp.format(name="surface"),
+            dpi=dpi,
+            bbox_inches="tight"
+        )
+        plt.close()
+
+    for k, v in values.items():
+        value = transformers[k](v)
+        print("ploting", k)
+        plot_value(value, os.path.join(save_path, "{name}_" + f"{k}.png"), levels)
+
+    # generate_vtp(
+    #     eval_fp=eval_fp,
+    #     coordinates_fp=coordinates_fp,
+    #     save_fp=os.path.join(save_path, "surface_{name}.vtp"),
+    #     **vtp_cfg
+    # )
+
+
+def plot_main(save_path: str, plot_cfg: Dict[str, Any], l2_weight: float):
+    coordinates_fp = os.path.join(save_path, "coordinates.pth")
+    coordinates = torch.load(coordinates_fp, map_location="cpu")
+
+    def identity(x):
+        return x
+
+    def norm(x: np.ndarray):
+        return x / x.max()
+
+    transformers = {
+        "loss": identity,
+        "err@1": identity,
+        "model_l2": identity,
+        "loss+l2": identity
+    }
+
+    x_len = len(coordinates["x_coordinate"])
+    y_len = len(coordinates["y_coordinate"])
+    accumulators = EvalAccumulators(x_len, y_len, transformers.keys())
+    iters = itertools.product(range(x_len), range(y_len))
+    for task_id, (x_id, y_id) in enumerate(iters):
+        fp = os.path.join(save_path, "eval", f"{task_id}.pkl")
+        values = torch.load(fp, map_location="cpu")
+        values["err@1"] = 100 * (1 - values["acc"][1])
+        values["loss+l2"] = values["loss"] + l2_weight * values["model_l2"]
+        accumulators.update(x_id, y_id, values)
+    values: Dict[str, np.ndarray] = accumulators.accumulate(to_numpy=True)
+
+    plot_landscape(
+        save_path=save_path,
+        x_coordinate=coordinates["x_coordinate"].numpy(),
+        y_coordinate=coordinates["y_coordinate"].numpy(),
+        transformers=transformers,
+        values=values,
+        levels=plot_cfg["levels"],
+        dpi=plot_cfg["dpi"]
+    )
 
 
 def main(args):
@@ -224,7 +330,8 @@ def main(args):
         # make sure listener is stopped
         logger_listener.stop()
 
-    plot(args)
+    logger.info("Plotting loss landscape")
+    plot_main(args.save_path, plot_cfg, l2_weight=args.l2_weight)
 
 
 if __name__ == "__main__":
@@ -236,9 +343,16 @@ if __name__ == "__main__":
     parser.add_argument("--seed", type=int, default=None)
     parser.add_argument("--num_worker", type=int, default=8)
     parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--l2_weight", type=float, default=5.0e-4)
     parser.add_argument("--use_train", action="store_true")
     parser.add_argument("--override", action="store_true")
+    parser.add_argument("--direct_plot", action="store_true")
     args = parser.parse_args()
     os.makedirs(args.save_path, exist_ok=True)
     os.makedirs(os.path.join(args.save_path, "eval"), exist_ok=True)
-    main(args)
+    if args.direct_plot:
+        global_cfg = get_cfg(args.cfg_fp)
+        plot_cfg: Dict[str, Any] = global_cfg["plot"]
+        plot_main(args.save_path, plot_cfg, args.l2_weight)
+    else:
+        main(args)
